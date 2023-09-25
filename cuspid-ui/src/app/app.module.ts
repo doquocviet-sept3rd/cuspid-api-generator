66f79e22fddb5228eefff9c66b2ddc85f85000b7
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2023-09-25 23:04:20 +0700

    Config tailwind.config.js.

--- cuspid-ui/src/app/app.module.ts
+++ cuspid-ui/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppTranslateModule } from './app.translate';
+
+/**
+ * @author Do Quoc Viet
+ */
 
 @NgModule({
   declarations: [
@@ -10,9 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppTranslateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
